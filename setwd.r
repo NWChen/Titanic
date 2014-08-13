e8cf09e965b05f41c4d167fd096aaedf99b74975
NWChen <neilwchen@gmail.com>
2014-08-13 12:25:28 -0400

    working on R model

---
+++ setwd.r
@@ -0,0 +1,4 @@
+setwd("C:/Users/Neil/Desktop/Research/titanic") #set working directory
+train <- read.csv("C:/Users/Neil/Desktop/Research/titanic/train.csv") #import training set
+test <- read.csv("C:/Users/Neil/Desktop/Research/titanic/test.csv") #import test sample
+
