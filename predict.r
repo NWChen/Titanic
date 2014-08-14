b92de53890b54c446fc07b6704fba83ffec02b41
Neil Chen <neilwchen@gmail.com>
2014-08-13 23:38:34 -0400

    modified file paths for ubuntu

--- predict.r
+++ predict.r
@@ -1,7 +1,23 @@
-setwd("C:/Users/Neil/Desktop/Research/titanic") #set working directory
-train <- read.csv("C:/Users/Neil/Desktop/Research/titanic/train.csv") #import training set
-test <- read.csv("C:/Users/Neil/Desktop/Research/titanic/test.csv") #import test sample
+#Windows
+#setwd("C:/Users/Neil/Desktop/Research/titanic") #set working directory
+#train <- read.csv("C:/Users/Neil/Desktop/Research/titanic/train.csv") #import training set
+#test <- read.csv("C:/Users/Neil/Desktop/Research/titanic/test.csv") #import test sample
 
-train$Child <- rep(0, length(train$Age)) #populate the Child dimension with 0s
+#Linux
+setwd("/home/neil/Desktop/Dev/Titanic")
+train <- read.csv("/home/neil/Desktop/Dev/Titanic/train.csv")
+test <- read.csv("/home/neil/Desktop/Dev/Titanic/test.csv")
+
+train$Child <- rep(0, length(train$Age)) #populate the Child dimension with 0sA
 train$Child[train$Age<=18] <- 1 #rows with Age<=18 are a 1 on the Child dimension
-aggregate(Survived ~ Child+Sex, data=train, FUN=sum) #set Survived to Child+Sex, looking at the train dataframe and performing the sum function
+
+#Survived depends on Child variables and Sex variables
+aggregate(Survived ~ Child+Sex, data=train, FUN=sum) #set Survived to Child+Sex subsets, looking at the train dataframe and performing the sum function
+aggregate(Survived ~ Child+Sex, data=train, FUN=length) #find total number of people in each subset (child/female, nonchild/female, child/male, nonchild/male)
+
+#calculate aggregated proportions
+p <- function(x){
+  return(sum(x)/length(x))
+  }
+
+aggregate(Survived ~ Child+Sex, data=train, FUN=p)
